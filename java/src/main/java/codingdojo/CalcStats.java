d16ae10ced734a61e36fe6f94983ca341377a831
emily <emily@bacheconsulting.com>
2023-01-17 16:13:50 +0100

    initial translation

---
+++ java/src/main/java/codingdojo/CalcStats.java
@@ -0,0 +1,41 @@
+package codingdojo;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CalcStats {
+
+    int minimum(Stream<Integer> numbers) {
+        List<Integer> numbersList = numbers.toList();
+        int min = numbersList.get(0);
+        for (int n : numbersList) {
+            if (n < min) {
+                min = n;
+            }
+        }
+        return min;
+    }
+
+    int maximum(Stream<Integer> numbers) {
+        List<Integer> numbersList = numbers.toList();
+        int max = numbersList.get(0);
+        for (int n : numbersList) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        return max;
+    }
+
+    float average(Stream<Integer> numbers) {
+        List<Integer> numbersList = numbers.toList();
+        if (numbersList.size() < 1) {
+            return 0;
+        }
+        float sum = 0;
+        for (int n : numbersList) {
+            sum += n;
+        }
+        return sum / (float)numbersList.size();
+    }
+}
