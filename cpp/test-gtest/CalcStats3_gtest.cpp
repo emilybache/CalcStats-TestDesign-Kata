969c479184161bddfa59da7b11a5b477984730c8
emily <emily@bacheconsulting.com>
2023-04-10 10:23:43 +0200

    improved 3rd exercise tests

--- cpp/test-gtest/CalcStats3_gtest.cpp
+++ cpp/test-gtest/CalcStats3_gtest.cpp
@@ -4,22 +4,34 @@
 
 using namespace std;
 
-TEST(CalcStats3Test, Count) {
-auto calcStats = CalcStats3({56, 2, 41});
-ASSERT_NO_THROW(calcStats.getReport());
+TEST(CalcStats3Test, NormalList) {
+    auto calcStats = CalcStats3({56, 2, 41});
+    const StatsReport &report = calcStats.getReport();
+    EXPECT_EQ(3, report.count);
+    EXPECT_EQ(56, report.maximum);
+    EXPECT_EQ(2, report.minimum);
+    EXPECT_NEAR(33, report.average, 0.001);
 }
 
-TEST(CalcStats3Test, Average) {
-auto calcStats = CalcStats3({6, 9, 15, -2, 92, 11});
-ASSERT_NO_THROW(calcStats.getReport());
+TEST(CalcStats3Test, OneItemList) {
+    auto calcStats = CalcStats3({42});
+    const StatsReport &report = calcStats.getReport();
+    EXPECT_EQ(1, report.count);
+    EXPECT_EQ(42, report.maximum);
+    EXPECT_EQ(42, report.minimum);
+    EXPECT_NEAR(42, report.average, 0.001);
 }
 
-TEST(CalcStats3Test, Minimum) {
-auto calcStats = CalcStats3({42, 3, -7});
-ASSERT_NO_THROW(calcStats.getReport());
+TEST(CalcStats3Test, EmptyList) {
+    auto calcStats = CalcStats3({});
+    ASSERT_THROW(calcStats.getReport(), std::invalid_argument);
 }
 
-TEST(CalcStats3Test, Maximum) {
-auto calcStats = CalcStats3({42});
-ASSERT_NO_THROW(calcStats.getReport());
+TEST(CalcStats3Test, SortedList) {
+    auto calcStats = CalcStats3({2, 42, 56});
+    const StatsReport &report = calcStats.getReport();
+    EXPECT_EQ(3, report.count);
+    EXPECT_EQ(56, report.maximum);
+    EXPECT_EQ(2, report.minimum);
+    EXPECT_NEAR(33, report.average, 0.001);
 }
