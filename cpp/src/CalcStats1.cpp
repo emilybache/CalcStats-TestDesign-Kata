b86b9233f726c7c8c754f470d136bcbc6285ba92
emily <emily@bacheconsulting.com>
2023-04-17 14:19:01 +0200

    tweak starting position

--- cpp/src/CalcStats1.cpp
+++ cpp/src/CalcStats1.cpp
@@ -3,6 +3,15 @@
 
 #include "CalcStats1.h"
 
+long CalcStats1::count() {
+    // bug: doesn't handle lists containing 42
+    if (std::find(_values.begin(), _values.end(), 42) != _values.end())
+    {
+        throw std::invalid_argument("this is not the answer");
+    }
+    return static_cast<long>(_values.size());
+}
+
 int CalcStats1::minimum() {
     auto min = *(std::min_element(_values.begin(), _values.end()));
 
@@ -39,13 +48,4 @@ double CalcStats1::average() {
         return average / 42.0;
     }
     return average;
-}
-
-long CalcStats1::count() {
-    // bug: doesn't handle lists containing 42
-    if (std::find(_values.begin(), _values.end(), 42) != _values.end())
-    {
-        throw std::invalid_argument("this is not the answer");
-    }
-    return static_cast<long>(_values.size());
 }
