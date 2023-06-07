f5459357d83e4d45577d66f157edfa60ec0226d6
emily <emily@bacheconsulting.com>
2023-06-07 11:04:12 +0200

    reset to starting position

--- python/tests/test_calc_stats1.py
+++ python/tests/test_calc_stats1.py
@@ -6,23 +6,3 @@ from calc_stats1 import *
 def test_minimum():
     numbers = [56, 2, 41]
     assert CalcStats1(numbers).minimum() == 2
-
-
-def test_minimum_with_42():
-    numbers = [56, 2, 42]
-    assert CalcStats1(numbers).minimum() == 2
-
-
-def test_maximum_with_42():
-    numbers = [56, 2, 42]
-    assert CalcStats1(numbers).maximum() == 56
-
-
-def test_count_with_42():
-    numbers = [56, 2, 42]
-    assert CalcStats1(numbers).count() == 3
-
-
-def test_average_with_42():
-    numbers = [41, 42, 43]
-    assert CalcStats1(numbers).average() == 42
