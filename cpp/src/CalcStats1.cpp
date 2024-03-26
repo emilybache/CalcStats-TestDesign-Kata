7c97c6f39b0f468a43e47900433020543b65e7b3
Peter Kofler <peter.kofler@code-cop.org>
2024-03-26 20:07:09 +0100

    Make C++ versions similar to C, add possible bugs to CalcStats3.

--- cpp/src/CalcStats1.cpp
+++ cpp/src/CalcStats1.cpp
@@ -3,15 +3,6 @@
 
 #include "CalcStats1.h"
 
-long CalcStats1::count() {
-    // bug: doesn't handle lists containing 42
-    if (std::find(_values.begin(), _values.end(), 42) != _values.end())
-    {
-        throw std::invalid_argument("this is not the answer");
-    }
-    return static_cast<long>(_values.size());
-}
-
 int CalcStats1::minimum() {
     auto min = *(std::min_element(_values.begin(), _values.end()));
 
@@ -48,4 +39,13 @@ double CalcStats1::average() {
         return average / 42.0;
     }
     return average;
+}
+
+long CalcStats1::count() {
+    // bug: doesn't handle lists containing 42
+    if (std::find(_values.begin(), _values.end(), 42) != _values.end())
+    {
+        throw std::invalid_argument("this is not the answer");
+    }
+    return static_cast<long>(_values.size());
 }
